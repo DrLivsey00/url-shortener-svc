b5f753d7a6b00485749ab4cb0fb23d0a1288d25d
DrLivsey00 <emihrin50@gmail.com>
2024-10-28 21:24:32 +0200

    Init routes

--- internal/service/router.go
+++ internal/service/router.go
@@ -15,7 +15,8 @@ func (s *service) router() chi.Router {
 		ape.CtxMiddleware(handlers.CtxLog(s.log)),
 	)
 	r.Route("/integrations/url-shortener-svc", func(r chi.Router) {
-
+		r.Get("/{alias}", s.handlers.RedirectHandler)
+		r.Post("/shorten", s.handlers.ShortenHandler)
 	})
 
 	return r
